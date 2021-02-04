4bd5c44a6106d3c389a2ccfb4a90566009b01534
Gonzalo Lopez andres <glopez-a@c2r8s1.42madrid.com>
2021-02-04 19:04:45 +0100

    hola que tal

--- discovery_piscine/rb04/ex04/redondear.rb
+++ discovery_piscine/rb04/ex04/redondear.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 print "Introduzca un número : "
-n1 = gets.to_i
-puts n1
+n1 = gets.to_f
+puts n1.ceil
--- discovery_piscine/rb04/ex05/up_low.rb
+++ discovery_piscine/rb04/ex05/up_low.rb
@@ -1,13 +1,3 @@
 #!/usr/bin/env ruby
 str = gets.chomp
-str = str.split("")
-i = 0
-while str[i]
-    if str[i][0] <= 'Z' && str[i][0] >= 'A'
-        str[i] = str[i].downcase
-    elsif str[i][0] <= 'z' && str[i][0] >= 'a'    
-        str[i] = str[i].upcase
-    end
-    i += 1
-end
-puts str.join
+puts str.swapcase
